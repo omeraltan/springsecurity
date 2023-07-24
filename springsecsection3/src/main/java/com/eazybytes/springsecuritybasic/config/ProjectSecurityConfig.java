0aa4d4c0c01af052e5a3736a16e1cb6045d1b413
omeraltan <omeraltan66@gmail.com>
2023-07-24 21:04:15 +0300

    Commit All

--- springsecsection3/src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -3,41 +3,67 @@ package com.eazybytes.springsecuritybasic.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
 
+          http.csrf().disable()
+              .authorizeHttpRequests()
+              .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+              .requestMatchers("/notices","/contact","/register").permitAll()
+              .and().formLogin()
+              .and().httpBasic();
+          return http.build();
+    }
 
-        // Below is the custom security configurations
+    /*Approach I where we use withDefaultPasswordEncoder() method while creating the user details*/
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("12345")
+            .authorities("admin")
+            .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("12345")
+            .authorities("read")
+            .build();
 
-          http.authorizeHttpRequests()
-                          .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                          .requestMatchers("/notices","/contact").permitAll()
-                          .and().formLogin()
-                          .and().httpBasic();
-                  return http.build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }*/
 
+    /*Approach I where we use NoOpPasswordEncoder Bean method while creating the user details*/
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withUsername("admin")
+            .password("12345")
+            .authorities("admin")
+            .build();
+        UserDetails user = User.withUsername("user")
+            .password("12345")
+            .authorities("read")
+            .build();
 
-        // Configuration to deny all the requests
+        return new InMemoryUserDetailsManager(admin,user);
+    }*/
 
-        /**
-         * http.authorizeHttpRequests().anyRequest().denyAll()
-         *                 .and().formLogin()
-         *                 .and().httpBasic();
-         *             return http.build();
-         */
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
 
-        // Configuration to permit all the requests
-
-        /**
-         * http.authorizeHttpRequests().anyRequest().permitAll()
-         *             .and().formLogin()
-         *             .and().httpBasic();
-         *         return http.build();
-         */
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
